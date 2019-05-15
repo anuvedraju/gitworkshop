b48a780d0ac663d0c9c5a60dc3b3d0bb2809b292
ANUVED RAJU <44817@fisat>
2019-05-15 04:56:06 -0400

    create a function to add two numbers

--- add.py
+++ add.py
@@ -7,6 +7,11 @@ def add2(num1,num2):
 def add3(num1,num2,num3):
     s= num1 + num2 +num3
     return s
+def add4(num1,num2,num3,num4):
+    sum=num1+num2+num3+num4)
+    return s
 
 print add2(2,3)
 print add3(3,4,5)
+print add4(3,4,5,6)
+
